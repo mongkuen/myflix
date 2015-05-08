2fa6e65a1405f037052810b247fc4318527660c7
Mong-Kuen Sun <mongkuen.sun@gmail.com>
2015-05-08 14:22:23 -0500

    Added capybara, launchy, and first feature specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,21 @@ describe User do
   it { should validate_uniqueness_of(:email) }
   it { should have_many(:reviews) }
   it { should have_many(:queue_items).order(:position) }
+
+  describe "#user_queued_video?" do
+    it "returns true if user has queued video" do
+      user = Fabricate(:user)
+      video = Fabricate(:video)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      expect(user.user_queued_video?(video)).to eq(true)
+    end
+
+    it "returns false if user has not queued video" do
+      user = Fabricate(:user)
+      video = Fabricate(:video)
+      video_2 = Fabricate(:video)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      expect(user.user_queued_video?(video_2)).to eq(false)
+    end
+  end
 end
